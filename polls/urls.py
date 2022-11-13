08dc1d9b1e948e7a0f4adfaf9005727f3ba64ae9
Raisa Ghosh <raisaghosh@Raisas-MacBook-Air.local>
2022-11-13 13:58:31 +0530

    week 4

--- polls/urls.py
+++ polls/urls.py
@@ -1,16 +1,31 @@
+# from django.urls import path
+
+# from . import views
+
+# urlpatterns = [
+#     # ex: /polls/
+#     path('', views.index, name='index'),
+#     #for COURSERA
+#      path('owner', views.owner, name='owner'),
+#     # ex: /polls/5/
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     # ex: /polls/5/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     # ex: /polls/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+
 from django.urls import path
 
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    #for COURSERA
-     path('owner', views.owner, name='owner'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    #FOR COURSERA
+    path('owner', views.owner, name='owner'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
